7152809a22023f3b268b5f7e6f6578aee6554b83
JDLEE <josephdlee08@gmail.com>
2020-12-18 19:02:31 -0500

    Create Bottles.java

---
+++ Bottles.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) {
+		
+		for(int i = 100; i >= 1; i--) {
+		    System.out.println(i + " bottles of pop on the wall");
+		    System.out.println("Take one down and pass it around");
+		}
+		
+		  
+		    
+		      
+		        
+		          
+		            
+		              System.out.println("No more bottles of pop on the wall");
+		 
+	}
+}
