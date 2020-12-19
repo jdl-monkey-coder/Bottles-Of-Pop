8ba76fade8a7aee38e3d873d64ef2c6c5013c176
JDLEE <josephdlee08@gmail.com>
2020-12-18 19:06:32 -0500

    Create Bottles.cpp

---
+++ Bottles.cpp
@@ -0,0 +1,20 @@
+int main()
+{
+    
+    for(int i = 100; i >= 1; i--) {
+        
+        cout << i << " bottles of pop on the wall\n";
+        cout << "Take one down and pass it around\n";
+    }
+
+
+
+
+
+
+
+
+
+                                                                        cout << "No more bottles of pop on the wall";
+    return 0;
+}
