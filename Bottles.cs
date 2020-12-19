5ff4342bf096e88a5299c8fcff43c03083fc29b0
JDLEE <josephdlee08@gmail.com>
2020-12-19 09:10:28 -0500

    Create Bottles.cs

---
+++ Bottles.cs
@@ -0,0 +1,19 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    
+    for(int i = 100; i >= 1; i--) {
+        
+        Console.WriteLine(i + " bottles of pop on the wall");
+        Console.WriteLine("Take one down and pass it around");
+    }
+    
+    
+    
+    
+    
+    
+                                                                                Console.WriteLine("No more bottles of pop on the wall");    
+    
+  }
+}
