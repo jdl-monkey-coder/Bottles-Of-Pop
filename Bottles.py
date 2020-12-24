3d535593f0a276756867e6fc4abb73533f8b526c
JDLEE <josephdlee08@gmail.com>
2020-12-24 10:23:34 -0500

    Update Bottles.py

--- Bottles.py
+++ Bottles.py
@@ -1,6 +1,6 @@
 #credit to BrainDead#6105 (albertopolijak) for the 'range(100, 0, -1):'
 
-for i in range(100, 0, -1):
+for i in range(99, 0, -1):
     print(str(i) + ' bottles of pop on the wall')
     print('Take one down and pass it around')
 
