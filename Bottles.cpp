90a27b4593c0d4b52e8fe2c9c4f701b8cb65e19e
JDLEE <josephdlee08@gmail.com>
2020-12-24 10:22:41 -0500

    Update Bottles.cpp

--- Bottles.cpp
+++ Bottles.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     
-    for(int i = 100; i >= 1; i--) {
+    for(int i = 99; i >= 1; i--) {
         
         cout << i << " bottles of pop on the wall\n";
         cout << "Take one down and pass it around\n";
