3f682f01d5e348f162ca6c15c93f629929f28d5d
JDLEE <josephdlee08@gmail.com>
2020-12-24 10:15:27 -0500

    Create Bottles.js

---
+++ Bottles.js
@@ -0,0 +1,19 @@
+let i;
+
+for(i = 100; i >= 1; i--) {
+    console.log(`${i} bottles of pop on the wall`);
+    console.log('Take one down and pass it around');
+}
+    
+        
+            
+                
+                    
+                        
+                            
+                                
+                                    
+                                        
+                                            
+                                                
+                                                console.log('No more bottles of pop on the wall');
