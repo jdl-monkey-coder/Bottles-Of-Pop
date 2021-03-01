470be0656a541a8870621dbcf78c2d829ae96026
joseph.L <josephdlee08@gmail.com>
2021-03-01 08:51:54 -0500

    Update Bottles.py
    Added f - string on Line 4

--- Bottles.py
+++ Bottles.py
@@ -1,7 +1,7 @@
 #credit to BrainDead#6105 (albertopolijak) for the 'range(100, 0, -1):'
 
 for i in range(99, 0, -1):
-    print(str(i) + ' bottles of pop on the wall')
+    print(f'{i} bottles of pop on the wall')
     print('Take one down and pass it around')
 
 print('No more bottles of pop on the wall')
